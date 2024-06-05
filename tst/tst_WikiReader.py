784919b8c607a76a1f285ee21ed5d9c97ea89e8d
andrijdemianczuk <andrij.demianczuk@gmail.com>
2024-06-04 20:57:04 -0600

    Adding test directive to Makefile

--- tst/tst_WikiReader.py
+++ tst/tst_WikiReader.py
@@ -1,4 +1,6 @@
 from src.WikiReader import search_wikipedia
+
+
 def test_search_wikipedia():
-    result = search_wikipedia (query="Goldendoodle", length=10)
+    result = search_wikipedia(query="Goldendoodle", length=10)
     assert "goldendoodle" in result
