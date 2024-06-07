31ad6db5efd461f0493a04648e10e292b5090617
andrijdemianczuk <andrij.demianczuk@gmail.com>
2024-06-07 12:15:08 -0600

    added cli via fire

--- cli-fire.py
+++ cli-fire.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 import fire
-# from src.WikiReader import summarize_wikipedia, search_wikipedia
-# from src.WikiReader import search_wikipedia
 import src.WikiReader
 
-
 if __name__ == "__main__":
+    #Rather than passing in a specific function like this:
+    #fire.Fire(src.WikiReader.summarize_wikipedia())
+
+    #We can instead pass in the library and use the function as an input parameter from the cli invocation itself.
     fire.Fire(src.WikiReader)
